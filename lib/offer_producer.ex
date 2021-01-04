c52205e10ad905d9ddc85b38224e309c3afe0743
Gustavo Oliveira <g.92oliveira@gmail.com>
2021-01-04 13:11:38 -0300

    dadfsf

--- lib/offer_producer.ex
+++ lib/offer_producer.ex
@@ -1,3 +1,31 @@
 defmodule OfferProducer do
+  @topic "offers"
 
+  def execute,
+    do:
+      %{
+        name: "Macbook pro #{:rand.uniform(1000)}",
+        discount: :rand.uniform(100),
+        price: :rand.uniform(100_000)
+      }
+      |> send_msg()
+
+  def executex,
+    do:
+      %{
+        name: "Xbox #{:rand.uniform(1000)}",
+        discount: :rand.uniform(100),
+        price: :rand.uniform(100_000)
+      }
+      |> send_msg()
+
+  def send_msg(payload) do
+    client_id = :offer_producer
+    hosts = [localhost: 9092]
+
+    :ok = :brod.start_client(hosts, client_id)
+    :ok = :brod.start_producer(client_id, @topic, [])
+
+    :brod.produce(client_id, @topic, 0, "", payload |> Jason.encode!())
+  end
 end
